34dc1cc71ee27a9afe2d3a175c1df04f56081630
Mia Marshall <mim7@njit.edu>
2019-07-24 22:17:22 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,19 +8,19 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
-  authorName: 'Anubhav Srivastava',
-  heading: 'Web Developer',
+  authorName: 'Mia Marshall',
+  heading: 'Artificial Intelligence/Machine Learning <br> Chatbot Development and Conversational Design <br> Web Development and Design <br> Web Application Security <br> EdTech: High School AP Computer Science',
   // social
   socialLinks: [
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com/mmrshll',
     },
     {
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
+      url: 'https://twitter.com/miaimarshall',
     },
     {
       icon: 'fa-facebook',
@@ -30,7 +30,7 @@ module.exports = {
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:miaimarshall@gmail.com',
     },
   ],
 };
